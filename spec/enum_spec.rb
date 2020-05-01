b2f67631cb3660dc8b1c957db3babb55c026f917
Osong Agberndifor <agberndifor7@gmail.com>
2020-05-01 12:04:35 +0100

    Fix lint errors

--- spec/enum_spec.rb
+++ spec/enum_spec.rb
@@ -2,17 +2,16 @@ require '../enum.rb'
 
 RSpec.describe Enumerable do
   let(:array1) { [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] }
-  let(:array2) { ['hello', 'hey', 'hi', 'holla', 'yea', 'good'] }
+  let(:array2) { %w[hello hey hi holla yea good] }
   let(:array3) { [1, 2, 3, 4] }
   let(:array4) { [2, 4, 6, 8] }
   let(:array5) { [2, 4, 6, 8, 'jersey', %w[why me cr7 oa7]] }
   let(:array6) { [4, 2, 3, 4, 4, 3, 5, 4, 4] }
-  let(:new_array) {[]}
+  let(:new_array) { [] }
 
   describe '#my_each' do
-
     it 'Passes each element to a new array based on some operation' do
-      array1.my_each { |ele| new_array << ele * 1}
+      array1.my_each { |ele| new_array << ele * 1 }
 
       expect(new_array).to eql(array1)
     end
@@ -26,37 +25,33 @@ RSpec.describe Enumerable do
     it 'Returns the Enumerator when no block is given' do
       expect(array1.my_each).to be_kind_of(Enumerator)
     end
-
   end
 
   describe '#my_each_with_index' do
-
     it 'Returns the Enumerator when no block is given' do
       expect(array1.my_each_with_index).to be_kind_of(Enumerator)
     end
 
     it 'Returns element plus index when argument is passed' do
-      array3.my_each_with_index { |ele, index| new_array << "#{index} : #{ele}"}
+      array3.my_each_with_index { |ele, index| new_array << "#{index} : #{ele}" }
 
-      expect(new_array).to eql(["0 : 1", "1 : 2", "2 : 3", "3 : 4"])
+      expect(new_array).to eql(['0 : 1', '1 : 2', '2 : 3', '3 : 4'])
     end
 
     it 'Return the index position and element when specific results are wanted' do
       array3.my_each_with_index { |ele, index| new_array << "#{index} : #{ele}" if ele.even? }
 
-      expect(new_array).to eql([ "1 : 2", "3 : 4"])
+      expect(new_array).to eql(['1 : 2', '3 : 4'])
     end
-
   end
 
   describe '#my_select' do
-
     it 'Returns specific data from the array based on the argument passed' do
       array2.my_select { |ele| new_array << ele if ele.include?('h') }
-      expect(new_array).to eq(['hello', 'hey', 'hi', 'holla'])
+      expect(new_array).to eq(%w[hello hey hi holla])
     end
 
-    it 'Returns new array with specific data based on arguments' do 
+    it 'Returns new array with specific data based on arguments' do
       array1.my_select { |ele| new_array << ele if ele.odd? && ele > 0 }
 
       expect(new_array). to eq([1, 3, 5, 7, 9])
@@ -65,11 +60,9 @@ RSpec.describe Enumerable do
     it 'Returns the Enumerator when no block is given' do
       expect(array1.my_select).to be_kind_of(Enumerator)
     end
-
   end
 
   describe '#my_all?' do
-    
     it "Returns false when all elements of the array don\'t\ have an \'h'\ in them" do
       expect(array2.my_all?(/[h]/)).to be false
     end
@@ -84,23 +77,20 @@ RSpec.describe Enumerable do
   end
 
   describe '#my_any?' do
-
     it "Returns false when none of the elements in the array contains a \'t'\ " do
       expect(array2.my_any?(/[z]/)).to be false
     end
 
     it "Returns true if any of the elements in the array contains the string \'ello'\ " do
-      expect(array2.my_any? { |ele| ele.include?('ello')} ).to be true
+      expect(array2.my_any? { |ele| ele.include?('ello') }).to be true
     end
 
     it 'Returns true if any of the elements is an Array' do
       expect(array5.my_any?(Array)).to be true
     end
-
   end
 
   describe '#my_none?' do
-    
     it "Returns true when none of the elements in the array contains a \'t'\ " do
       expect(array2.my_none?(/[t]/)).to be true
     end
@@ -112,11 +102,9 @@ RSpec.describe Enumerable do
     it 'Returns false if none of the elements in the array is greater than 7' do
       expect(array1.my_none? { |ele| ele > 7 }).to be false
     end
-
   end
 
   describe '#my_count' do
-    
     it 'Returns the number of elements in the array(array size)' do
       expect(array1.my_count).to eql(10)
     end
@@ -131,9 +119,8 @@ RSpec.describe Enumerable do
   end
 
   describe '#my_map' do
-
-    it "Creates a new array containing the values returned after executing the code in the given block" do
-      expect(array3.my_map {|x| x + 1}).to eql([2, 3, 4, 5])
+    it 'Creates a new array containing the values returned after executing the code in the given block' do
+      expect(array3.my_map { |x| x + 1 }).to eql([2, 3, 4, 5])
     end
 
     it 'Returns an array of elements where even numbers have 100 added to them and odd numbers 200 added to them' do
@@ -143,17 +130,15 @@ RSpec.describe Enumerable do
     it 'Returns the Enumerator when no block is given' do
       expect(array1.my_map).to be_kind_of(Enumerator)
     end
-
   end
 
   describe '#my_inject' do
-    
-    it "Returns the sum of all elements in an array when block is given" do
-      expect(array1.my_inject {|a, b| a + b}).to eql(45)
+    it 'Returns the sum of all elements in an array when block is given' do
+      expect(array1.my_inject { |a, b| a + b }).to eql(45)
     end
 
-    it "Returns the sum of all elements in an array when block is given plus the accumulator value when it is passed as an argument" do
-      expect(array1.my_inject(10) {|a, b| a + b}).to eql(55)
+    it 'Returns the sum of all elements in an array plus the accumulator value when it is passed as an argument' do
+      expect(array1.my_inject(10) { |a, b| a + b }).to eql(55)
     end
 
     it 'Returns the product of all elements if argument is passed as a String' do
@@ -167,7 +152,5 @@ RSpec.describe Enumerable do
     it 'Returns the product of all elements multiplied by the accumulator value given' do
       expect(array3.my_inject(10, :*)).to eq(240)
     end
-
   end
-
 end
